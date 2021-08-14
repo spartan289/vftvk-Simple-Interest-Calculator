29103800f8f5fd45e6a7e49ae0c44c99f454491e
spartan289 <60476934+spartan289@users.noreply.github.com>
2021-08-14 11:06:48 +0530

    submit

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var p = document.getElementById("principal").value;
+    var r = document.getElementById("rate").value;
+    var t = document.getElementById("years").value;
+
+    var complete = p*r*t;
+    compute = compute/100
+    var year = Number(t)+2020
+    document.getElementById("result").innerText += `If you deposit ${p},\nat an interest rate of ${r}%.\nYou will recieve an amount of ${complete},\nin the year ${year}`;
 }
-        
+function updateTextInput(val) {
+    document.getElementById('demo').innerText=`${val}%`; 
+  }
